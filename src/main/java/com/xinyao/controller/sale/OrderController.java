37a64b52978b073c898293c3d21e895d3f0320ce
wangxbgg <2317216546@qq.com>
2022-07-12 10:10:06 +0800

    修改

--- src/main/java/com/xinyao/controller/sale/OrderController.java
+++ src/main/java/com/xinyao/controller/sale/OrderController.java
@@ -58,9 +58,9 @@ public class OrderController {
     }
 
     @ApiOperation("订单---取消订单")
-    @PostMapping("cancelOrder")
-    public R cancelOrder(@RequestBody OrderVo orderVo){
-        return R.isOk(orderService.cancelOrder(orderVo), "取消");
+    @GetMapping("cancelOrder")
+    public R cancelOrder(Long id){
+        return R.isOk(orderService.cancelOrder(id), "取消");
     }
 
 }
